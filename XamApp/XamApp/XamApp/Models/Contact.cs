f051db78d5396a6d52f502cadbfdff181d5fa2e4
tereza <tstankova@deloittece.com>
2019-01-14 11:56:43 +0100

    feat: add sqlite-net-pcl

---
+++ XamApp/XamApp/XamApp/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace XamApp.Models
+{
+    class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [MaxLength(250), Unique]
+        public string Name { get; set; }
+        [MaxLength(250), Unique]
+        public string PhoneNumber { get; set; }
+    }
+}
